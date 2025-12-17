2c31b313f6032ca2402770c1f2b73fe6e809842e
jbannon <j.bannon90@gmail.com>
2025-12-17 11:01:13 -0500

    good hdd push, snakemake still needed

--- workflow/scripts/construct_MAE.R
+++ workflow/scripts/construct_MAE.R
@@ -1,26 +1,63 @@
 library(MultiAssayExperiment)
+library(Matrix)
+library(SummarizedExperiment)
 
 
-exp.names <- c("BindingDB",'Bioassays', 'ToxCast', 'Tox21', 'ClinTox','Sider')
+
+
 colData <- read.csv("data/procdata/colData.csv")
-# fps <- read.csv("data/procdata/experiments/fingerprints.csv")
-bdb <- read.csv("data/procdata/experiments/binding_db.csv",check.names=FALSE)
-bioassays <- read.csv("data/procdata/experiments/filtered_bioassays.csv",check.names=FALSE)
-toxcast <- read.csv("data/procdata/experiments/toxcast.csv",check.names = FALSE)
-tox21 <- read.csv("data/procdata/experiments/tox21.csv",check.names = FALSE)
-clintox <- read.csv("data/procdata/experiments/clintox.csv",check.names = FALSE)
-sider <- read.csv("data/procdata/experiments/sider.csv",check.names = FALSE)
+rownames(colData) <- colData$Pubchem.CID
+colData <- DataFrame(colData, row.names = rownames(colData))
 
-el = ExperimentList(
-	 BindingDB= bdb,
-	 Bioassays = bioassays,
-	 ToxCast = toxcast,
-	 Tox21 = tox21,
-	 ClinTox = clintox,
-	 SIDER = sider
-	 )
 
-sml <- lapply(el, function(se) { 
+bdb <- read.csv("data/procdata/experiments/binding_db.csv",row.names = 1,check.names=FALSE)
+bioassays <- read.csv("data/procdata/experiments/filtered_bioassays.csv",row.names = 2,check.names=FALSE)
+toxcast <- read.csv("data/procdata/experiments/toxcast.csv",row.names = 1,check.names = FALSE)
+colnames(toxcast) <- sub("\\.0$", "", as.character(colnames(toxcast)))
+
+tox21 <- read.csv("data/procdata/experiments/tox21.csv",row.names = 1, check.names = FALSE)
+clintox <- read.csv("data/procdata/experiments/clintox.csv",row.names =1, check.names = FALSE)
+sider <- read.csv("data/procdata/experiments/sider.csv",row.names =1, check.names = FALSE)
+
+
+
+
+
+fingerprint.files <- list.files("data/procdata/experiments/fingerprints/")
+fp_assays <- list()
+
+for (fp.file in fingerprint.files){
+	# print(paste0("data/procdata/experiments/fingerprints/",fp.file))
+	fp.data <- read.csv(
+		paste0("data/procdata/experiments/fingerprints/",fp.file),
+		check.names = FALSE)
+	fp.data <- as(fp.data, "sparseMatrix")
+	
+	fp.stem <- unlist(strsplit(fp.file,"\\."))
+	fp.name <- paste(c("fingerprint",as.character(fp.stem[-length(fp.stem)])),
+		sep="",collapse=".")
+	fp_assays[[fp.name]] <- SummarizedExperiment(assays = list(fp.name = fp.data))
+}
+
+
+
+
+experiments <- c(
+	list( 
+SIDER = SummarizedExperiment(assays = list(SIDER = as.matrix(sider))),
+BindingDB = SummarizedExperiment(assays = list(BindingDB = as.matrix(bdb))),
+Bioassays = SummarizedExperiment(assays = list(Bioassays = bioassays)),
+Tox21 = SummarizedExperiment(assays = list(Tox21 = tox21)),
+ToxCast = SummarizedExperiment(assays = list(ToxCast = toxcast)),
+ClinTox = SummarizedExperiment(assays = list(ClinTox = clintox))
+),
+	fp_assays
+	)
+
+
+experimentList <- as(experiments,"ExperimentList")
+
+sampleMapList <- lapply(experiments, function(se) { 
 	data.frame(
     primary = colnames(se),
     colname = colnames(se),
@@ -28,5 +65,10 @@ sml <- lapply(el, function(se) {
   )
 })
 
-mae = MultiAssayExperiment(experiments = el, colData = colData, sampleMap = listToMap(sml))
+mae = MultiAssayExperiment(
+	experiments = experimentList, 
+	colData = colData, 
+	sampleMap = listToMap(sampleMapList)
+	)
 saveRDS(mae,"data/results/hdd.RDS")
+print(mae)
--- workflow/scripts/make_fingerprints.py
+++ workflow/scripts/make_fingerprints.py
@@ -5,7 +5,7 @@ from typing import List, Dict
 from itertools import product
 from rdkit.Chem import AllChem
 import pandas as pd  
-
+from pathlib import Path
 
 def make_fingerprint_generators(
 	radius_list:List[int],
@@ -31,24 +31,39 @@ def main(
 	
 	fingerprint_generators = make_fingerprint_generators(radius_list,dimension_list)
 	fp_data = {k:defaultdict(list) for k in fingerprint_generators}
-	fp_data = defaultdict(list)
+	# fp_data = defaultdict(list)
 	colData = pd.read_csv(dirs.PROCDATA / "colData.csv",usecols = ['Pubchem CID', 'SMILES'])
-
+	
+	outpath = dirs.PROCDATA / "experiments"/ "fingerprints"
+	Path(outpath).mkdir(parents=True,exist_ok=True)
+	
+	# Make the fingerpints
 	for idx, row in colData.iterrows():
 		cid = row['Pubchem CID']
 		smiles_str = row['SMILES']
-	# Make the fingerpints
+		
 		rdk_mol = Chem.MolFromSmiles(smiles_str)
-		fp_data['CID'].append(cid)
+		
 
 		for fp_type in fingerprint_generators:
+			fp_data[fp_type]['CID'].append(cid)
 			generator = fingerprint_generators[fp_type]
 			fp = generator.GetCountFingerprintAsNumPy(rdk_mol)
-			fp_data[fp_type].append(fp)
+			for j in  range(fp.shape[0]):
+				vname = f"V{j+1}"
+				fp_data[fp_type][vname].append(fp[j])
+	
+	for fp_type in fp_data:
+		fp_str = ".".join(fp_type.replace("(",".").replace(")","").split(","))
+		
+		fp_matrix = pd.DataFrame(fp_data[fp_type])
+		fp_matrix = fp_matrix.transpose()
+		
+		fp_matrix = fp_matrix.rename(columns = fp_matrix.iloc[0])
 
-
-	fp_data = pd.DataFrame(fp_data)
-	fp_data.to_csv(dirs.PROCDATA / "experiments" / "fingerprints.csv")
+		fp_matrix = fp_matrix.iloc[1:,]
+		fp_matrix = fp_matrix.reset_index(drop=True)
+		fp_matrix.to_csv(outpath /f"{fp_str}.csv",index=False)
 		
 
 if __name__ == '__main__':
