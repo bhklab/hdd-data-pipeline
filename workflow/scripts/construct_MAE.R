23fae5e54969632698b547f6e9665ecae720c9c3
Michael Tran <bhklab.michaeltran@gmail.com>
2025-12-19 16:25:29 -0500

    style: air format R script

--- workflow/scripts/construct_MAE.R
+++ workflow/scripts/construct_MAE.R
@@ -3,62 +3,83 @@ library(Matrix)
 library(SummarizedExperiment)
 
 
-
-
 colData <- read.csv("data/procdata/colData.csv")
 rownames(colData) <- colData$Pubchem.CID
 colData <- DataFrame(colData, row.names = rownames(colData))
 
 
-bdb <- read.csv("data/procdata/experiments/binding_db.csv",row.names = 1,check.names=FALSE)
-bioassays <- read.csv("data/procdata/experiments/filtered_bioassays.csv",row.names = 2,check.names=FALSE)
-toxcast <- read.csv("data/procdata/experiments/toxcast.csv",row.names = 1,check.names = FALSE)
+bdb <- read.csv(
+  "data/procdata/experiments/binding_db.csv",
+  row.names = 1,
+  check.names = FALSE
+)
+bioassays <- read.csv(
+  "data/procdata/experiments/bioassays.csv",
+  row.names = 1,
+  check.names = FALSE
+)
+toxcast <- read.csv(
+  "data/procdata/experiments/toxcast.csv",
+  row.names = 1,
+  check.names = FALSE
+)
 colnames(toxcast) <- sub("\\.0$", "", as.character(colnames(toxcast)))
 
-tox21 <- read.csv("data/procdata/experiments/tox21.csv",row.names = 1, check.names = FALSE)
-clintox <- read.csv("data/procdata/experiments/clintox.csv",row.names =1, check.names = FALSE)
-sider <- read.csv("data/procdata/experiments/sider.csv",row.names =1, check.names = FALSE)
-
-
-
+tox21 <- read.csv(
+  "data/procdata/experiments/tox21.csv",
+  row.names = 1,
+  check.names = FALSE
+)
+clintox <- read.csv(
+  "data/procdata/experiments/clintox.csv",
+  row.names = 1,
+  check.names = FALSE
+)
+sider <- read.csv(
+  "data/procdata/experiments/sider.csv",
+  row.names = 1,
+  check.names = FALSE
+)
 
 
 fingerprint.files <- list.files("data/procdata/experiments/fingerprints/")
 fp_assays <- list()
 
-for (fp.file in fingerprint.files){
-	# print(paste0("data/procdata/experiments/fingerprints/",fp.file))
-	fp.data <- read.csv(
-		paste0("data/procdata/experiments/fingerprints/",fp.file),
-		check.names = FALSE)
-	fp.data <- as(fp.data, "sparseMatrix")
-	
-	fp.stem <- unlist(strsplit(fp.file,"\\."))
-	fp.name <- paste(c("fingerprint",as.character(fp.stem[-length(fp.stem)])),
-		sep="",collapse=".")
-	fp_assays[[fp.name]] <- SummarizedExperiment(assays = list(fp.name = fp.data))
+for (fp.file in fingerprint.files) {
+  # print(paste0("data/procdata/experiments/fingerprints/",fp.file))
+  fp.data <- read.csv(
+    paste0("data/procdata/experiments/fingerprints/", fp.file),
+    check.names = FALSE
+  )
+  fp.data <- as(fp.data, "sparseMatrix")
+
+  fp.stem <- unlist(strsplit(fp.file, "\\."))
+  fp.name <- paste(
+    c("fingerprint", as.character(fp.stem[-length(fp.stem)])),
+    sep = "",
+    collapse = "."
+  )
+  fp_assays[[fp.name]] <- SummarizedExperiment(assays = list(fp.name = fp.data))
 }
 
 
-
-
 experiments <- c(
-	list( 
-SIDER = SummarizedExperiment(assays = list(SIDER = as.matrix(sider))),
-BindingDB = SummarizedExperiment(assays = list(BindingDB = as.matrix(bdb))),
-Bioassays = SummarizedExperiment(assays = list(Bioassays = bioassays)),
-Tox21 = SummarizedExperiment(assays = list(Tox21 = tox21)),
-ToxCast = SummarizedExperiment(assays = list(ToxCast = toxcast)),
-ClinTox = SummarizedExperiment(assays = list(ClinTox = clintox))
-),
-	fp_assays
-	)
+  list(
+    SIDER = SummarizedExperiment(assays = list(SIDER = as.matrix(sider))),
+    BindingDB = SummarizedExperiment(assays = list(BindingDB = as.matrix(bdb))),
+    Bioassays = SummarizedExperiment(assays = list(Bioassays = bioassays)),
+    Tox21 = SummarizedExperiment(assays = list(Tox21 = tox21)),
+    ToxCast = SummarizedExperiment(assays = list(ToxCast = toxcast)),
+    ClinTox = SummarizedExperiment(assays = list(ClinTox = clintox))
+  ),
+  fp_assays
+)
 
 
-experimentList <- as(experiments,"ExperimentList")
+experimentList <- as(experiments, "ExperimentList")
 
-sampleMapList <- lapply(experiments, function(se) { 
-	data.frame(
+sampleMapList <- lapply(experiments, function(se) {
+  data.frame(
     primary = colnames(se),
     colname = colnames(se),
     stringsAsFactors = FALSE
@@ -66,9 +87,9 @@ sampleMapList <- lapply(experiments, function(se) {
 })
 
 mae = MultiAssayExperiment(
-	experiments = experimentList, 
-	colData = colData, 
-	sampleMap = listToMap(sampleMapList)
-	)
-saveRDS(mae,"data/results/hdd.RDS")
+  experiments = experimentList,
+  colData = colData,
+  sampleMap = listToMap(sampleMapList)
+)
+saveRDS(mae, "data/results/hdd.RDS")
 print(mae)
